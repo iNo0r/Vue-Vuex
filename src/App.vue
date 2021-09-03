b042a3493bf97fcefdf48120fa59254f4b306746
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-03 04:18:37 +0100

    initiating the store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+  <!-- simple way to reach out our state mangment "store" -->
+    <h3>{{ $store.state.counter }}</h3>
+  </base-container>
 </template>
 
 <script>
@@ -7,8 +10,8 @@ import BaseContainer from './components/BaseContainer.vue';
 
 export default {
   components: {
-    BaseContainer,
-  },
+    BaseContainer
+  }
 };
 </script>
 
