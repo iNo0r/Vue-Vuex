a2a3c1e704f6ba3185f193845f66740595d618b3
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-04 02:17:34 +0100

    organizing code using modules

--- src/main.js
+++ src/main.js
@@ -3,36 +3,22 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const store = createStore({
-  // state is equal to data
+const counterMoudule = {
   state() {
     return {
-      counter: 0,
-      isLoged: false
+      counter: 0
     };
   },
-  //   mutations are equal to methods
   mutations: {
-    // state down here is the current state
     increment(state) {
       state.counter = state.counter + 1;
     },
-    // payload is and argument passes data to the funciton
+
     increase(state, payload) {
       state.counter = state.counter + payload.value;
-    },
-    logIn(state) {
-      state.isLoged = true;
-    },
-    logOut(state) {
-      state.isLoged = false;
     }
   },
-  //   getters are similar to computed
   getters: {
-    // state : to reach state obvously, getters: to reach other getters
-    //   in case of the value of this getter depending on oth getters
-    //   finalCounter(state,getters){
     finalCounter(state) {
       return state.counter * 2;
     },
@@ -45,36 +31,47 @@ const store = createStore({
       } else {
         return finalCounter;
       }
-    },
-    getIsLoged(state) {
-      return state.isLoged;
     }
   },
-  //   mutations is responsible to effect the state, it is not allowed to have an asynchronous
-  //  functions, only synchronous functions .
-  // component should triggers actions, actions will handle the asychronous part,
-  //  and then will commit a mutation.
-  // it is a good practice to use actions instead of mutations
-  // to make sure we never put asynhronous code accedintly into mutations.
   actions: {
-    //we can use the same name as a mutations
-    // increase(context) {
-    // context help is to reach mutations
-    //   we can pass a payload too
-    //   context.commit("increase",payload);
-    // }
     increment(context) {
       context.commit('increment');
     },
-    //making an asynchronous example
-    // context = similar to this.$store
-    // context has commit,dispatch,state and getters
-    // you should not mutate a state from actions always use
     increase(context, payload) {
       setTimeout(function() {
         context.commit('increase', payload);
       }, 2000);
+    }
+  }
+};
+const store = createStore({
+  //modules is property allow us to outsource code
+  modules: {
+    // name is optional
+    numbersRelated: counterMoudule
+  },
+  // state is equal to data
+  state() {
+    return {
+      counter: 0,
+      isLoged: false
+    };
+  },
+  //   mutations are equal to methods
+  mutations: {
+    logIn(state) {
+      state.isLoged = true;
     },
+    logOut(state) {
+      state.isLoged = false;
+    }
+  },
+  getters: {
+    getIsLoged(state) {
+      return state.isLoged;
+    }
+  },
+  actions: {
     logIn(context) {
       context.commit('logIn');
     },
